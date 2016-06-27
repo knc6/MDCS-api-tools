1d627d8df500bd88fb3ffae98f998c2d7c574f5a
Logan Ward <ward.logan.t@gmail.com>
2016-06-27 10:51:14 -0600

    Added comments to explorer operations

--- mdcs/explore.py
+++ mdcs/explore.py
@@ -3,6 +3,22 @@ import requests
 from collections import OrderedDict
 
 def select_all(host,user,pswd,cert=None,format=None):
+    """Get all data from the MDCS server
+    
+    Inputs:
+        host - string, URL of MDCS instance
+        user - string, username of desired account on MDCS server
+        pswd - string, password of desired account on MDCS server
+        cert - string, path to authentication certificate
+        format - string, format of data (can be xml or json)
+    Output:
+        lists where each entry is a dictionary describing entries that match the query.
+        dictionaries each have the keys:
+            title - title of the entry
+            schema - ID of the schema that describes the entry
+            content - content of the entry in either xml or json format
+            _id - ID number of the entry
+    """
     url = host.strip("/") + "/rest/explore/select/all"
     params = dict()
     if format: params['dataformat'] = format
@@ -10,6 +26,25 @@ def select_all(host,user,pswd,cert=None,format=None):
     return r.json(object_pairs_hook=OrderedDict)
 
 def select(host,user,pswd,cert=None,format=None,ID=None,template=None,title=None):
+    """Get all data that fits a certain simple query
+    
+    Inputs:
+        host - string, URL of MDCS instance
+        user - string, username of desired account on MDCS server
+        pswd - string, password of desired account on MDCS server
+        cert - string, path to authentication certificate
+        format - string, format of data (can be xml or json)
+        template - string, ID of the schema for particular data
+        ID - string, ID of entry to be retrieved
+        title - string, title of data to be retrieved
+    Output:
+        list of all entries in the database
+        dictionaries each have the keys:
+            title - title of the entry
+            schema - ID of the schema that describes the entry
+            content - content of the entry in either xml or json format
+            _id - ID number of the entry
+    """
     url = host.strip("/") + "/rest/explore/select"
     params = dict()
     if format:   params['dataformat'] = format
@@ -42,6 +77,23 @@ def delete(ID,host,user,pswd,cert=None):
         return r.json()
     
 def query(host,user,pswd,cert=None,format=None,query=None,repositories=None):
+    """Query by example.
+    
+    Input:
+        host - string, URL of MDCS instance
+        user - string, username of desired account on MDCS server
+        pswd - string, password of desired account on MDCS server
+        cert - string, path to authentication certificate
+        format - string, format of data (can be xml or json)
+        respositories - string, lists of names of other repositories to be 
+    Output:
+        lists where each entry is a dictionary describing entries that match the query.
+        dictionaries each have the keys:
+            title - title of the entry
+            schema - ID of the schema that describes the entry
+            content - content of the entry in either xml or json format
+            _id - ID number of the entry
+    """
     url = host.strip("/") + "/rest/explore/query-by-example"
     data = dict()
     if format: data['dataformat'] = format
